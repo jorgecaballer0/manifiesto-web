2c5d6261f6d5b784491bcd87f08032092f3b09b7
Jorge Caballero <jorge.caballero.31.10.98@gmail.com>
2022-08-25 19:04:59 -0300

    Rutas

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import Home from "./components/Home/Home";
 import Services from "./components/Services/Services";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Footer from "./components/Footer/Footer";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Home />
-      {/*** Servicios ***/}
-      <Services />
-      {/*** Portfolio ***/}
-      <Portfolio />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        {/*** Servicios ***/}
+        <Route exact path="/servicios" element={<Services />} />
+        {/*** Portfolio ***/}
+        <Route exact path="/portfolio" element={<Portfolio />} />
+      </Routes>
       <Footer />
     </>
   );
